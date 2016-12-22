e6bf0d4c0ca24c7b8118b8619808ef473f4bdda4
sfanxiang <sfanxiang@gmail.com>
2016-12-22 22:14:57 +0800

    add sudoku.c

---
+++ sudoku.c
@@ -0,0 +1,3 @@
+/* from https://www.quora.com/What-is-the-most-complex-line-of-C-code-you-have-created-or-encountered */
+#define V ;for(n=0;o=q[n],n<81;n++)
+int o,n,q[];d(i,a,j){V i=o?i:n;V a|=(n-i)%9&&n/9-i/9&&i/27^n/27|i%9/3^n%9/3?0:1<<o;for(j=9;q[i]=j;)i=1<<j--&~a&&d(n,0)?n:i;return n==i;}main(m){for(scanf("%d",&m);m--;){V scanf("%d",q+n);d(n,0)V printf("%d ",o);}}
